6079bb9da5f6535257792d866443a9938738624a
철이 <lomi525@gmail.com>
2017-06-25 10:30:17 +0900

    SplitPane 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+// Pleas note the module is no in the root
+import { SplitPaneModule } from 'ng2-split-pane/lib/ng2-split-pane';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SplitPaneModule
   ],
   providers: [],
   bootstrap: [AppComponent]
