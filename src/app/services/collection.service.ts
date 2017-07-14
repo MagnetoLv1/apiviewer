c29c5ad1c55c5465be4b64df27aff9e50f3b0891
김광철 <anyman@afreecatv.com>
2017-07-14 10:26:46 +0900

    firebase 추가

--- src/app/services/collection.service.ts
+++ src/app/services/collection.service.ts
@@ -1,24 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response, RequestOptions, RequestOptionsArgs } from '@angular/http';
+import * as firebase from 'firebase';
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class CollectionService {
 
   headers: Headers;
+  app: firebase.app.App;
   constructor(private http: Http) {
     this.http = http;
     this.headers = new Headers();
     this.headers.append('Content-Type', 'multipart/form-data');
     this.headers.append('Access-Control-Allow-Origin', '*');
 
+    this.app = firebase.initializeApp({
+      apiKey: "AIzaSyBM_wKB_LYG2bdF-iFYaIaFHNcgetNCFPI",
+      authDomain: "apiviewer.firebaseapp.com",
+      databaseURL: "https://apiviewer.firebaseio.com",
+      projectId: "apiviewer",
+      storageBucket: "apiviewer.appspot.com",
+      messagingSenderId: "293629322300"
+    });
 
-    
+
   }
 
 
   getCollection() {
-    console.log('login');
+    var apiRef: firebase.database.Reference = firebase.database().ref('/api');
+    apiRef.on('value', function (snapshot) {
+      console.log(222,snapshot.val());
+    });
 
     const options = new RequestOptions({
       headers: this.headers
